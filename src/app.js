2e7f2e11cb1ec6f3dab77dbde0294a319124a088
Paul McKellar <paul.mckellar@gmail.com>
2018-07-25 10:23:11 -0700

    cleanup

--- src/app.js
+++ src/app.js
@@ -3,12 +3,9 @@ import Movies from './movies.jsx'
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// ReactDOM.render(React.createElement(Movies, {}, null), document.getElementById('app'));
-
 ReactDOM.render(
-  React.createElement(Movies, {toWhat: 'World'}, null),
+  React.createElement(Movies, {}, null),
   document.getElementById('app')
 );
-
 
 document.querySelectorAll('title')[0].textContent = "IMDB Top 250";
