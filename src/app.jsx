a16ce00991ec2a5898b992d232ce12a811d59aa9
Paul McKellar <paul.mckellar@gmail.com>
2018-07-25 13:44:50 -0700

    always in order

--- src/app.jsx
+++ src/app.jsx
@@ -19,242 +19,242 @@ var moviesOrder = [
   "The Lord of the Rings: The Fellowship of the Ring (2001)",
   "One Flew Over the Cuckoo's Nest (1975)",
   "Inception (2010)",
-  // "Goodfellas (1990)",
-  // "Star Wars (1977)",
-  // "Seven Samurai (1954)",
-  // "Forrest Gump (1994)",
-  // "The Matrix (1999)",
-  // "The Lord of the Rings: The Two Towers (2002)",
-  // "City of God (2002)",
-  // "Se7en (1995)",
-  // "The Silence of the Lambs (1991)",
-  // "Once Upon a Time in the West (1968)",
-  // "Casablanca (1942)",
-  // "The Usual Suspects (1995)",
-  // "Raiders of the Lost Ark (1981)",
-  // "Rear Window (1954)",
-  // "It's a Wonderful Life (1946)",
-  // "Psycho (1960)",
-  // "Léon: The Professional (1994)",
-  // "Sunset Blvd. (1950)",
-  // "American History X (1998)",
-  // "Apocalypse Now (1979)",
-  // "Terminator 2: Judgment Day (1991)",
-  // "Memento (2000)",
-  // "Saving Private Ryan (1998)",
-  // "City Lights (1931)",
-  // "Dr. Strangelove or: How I Learned to Stop Worrying and Love the Bomb (1964)",
-  // "Alien (1979)",
-  // "Spirited Away (2001)",
-  // "Modern Times (1936)",
-  // "North by Northwest (1959)",
-  // "Back to the Future (1985)",
-  // "Citizen Kane (1941)",
-  // "Life Is Beautiful (1997)",
-  // "The Shining (1980)",
-  // "The Pianist (2002)",
-  // "The Departed (2006)",
-  // "M (1931)",
-  // "Vertigo (1958)",
-  // "Django Unchained (2012)",
-  // "Paths of Glory (1957)",
-  // "The Dark Knight Rises (2012)",
-  // "Taxi Driver (1976)",
-  // "American Beauty (1999)",
-  // "Aliens (1986)",
-  // "Double Indemnity (1944)",
-  // "The Intouchables (2011)",
-  // "WALL·E (2008)",
-  // "Gladiator (2000)",
-  // "The Green Mile (1999)",
-  // "Toy Story 3 (2010)",
-  // "The Lives of Others (2006)",
-  // "The Great Dictator (1940)",
-  // "A Clockwork Orange (1971)",
-  // "The Prestige (2006)",
-  // "Amélie (2001)",
-  // "Lawrence of Arabia (1962)",
-  // "To Kill a Mockingbird (1962)",
-  // "Reservoir Dogs (1992)",
-  // "Das Boot (1981)",
-  // "Cinema Paradiso (1988)",
-  // "The Lion King (1994)",
-  // "The Treasure of the Sierra Madre (1948)",
-  // "The Third Man (1949)",
-  // "Requiem for a Dream (2000)",
-  // "Star Wars: Episode VI - Return of the Jedi (1983)",
-  // "Once Upon a Time in America (1984)",
-  // "Eternal Sunshine of the Spotless Mind (2004)",
-  // "Full Metal Jacket (1987)",
-  // "Braveheart (1995)",
-  // "Oldboy (2003)",
-  // "L.A. Confidential (1997)",
-  // "Chinatown (1974)",
-  // "Singin' in the Rain (1952)",
-  // "Bicycle Thieves (1948)",
-  // "Monty Python and the Holy Grail (1975)",
-  // "Some Like It Hot (1959)",
-  // "Metropolis (1927)",
-  // "Princess Mononoke (1997)",
-  // "Rashomon (1950)",
-  // "Amadeus (1984)",
-  // "2001: A Space Odyssey (1968)",
-  // "The Sting (1973)",
-  // "All About Eve (1950)",
-  // "The Apartment (1960)",
-  // "Unforgiven (1992)",
-  // "Indiana Jones and the Last Crusade (1989)",
-  // "Grave of the Fireflies (1988)",
-  // "Witness for the Prosecution (1957)",
-  // "Raging Bull (1980)",
-  // "The Bridge on the River Kwai (1957)",
-  // "Die Hard (1988)",
-  // "Batman Begins (2005)",
-  // "A Separation (2011)",
-  // "Yojimbo (1961)",
-  // "Inglourious Basterds (2009)",
-  // "For a Few Dollars More (1965)",
-  // "Snatch. (2000)",
-  // "The Great Escape (1963)",
-  // "Toy Story (1995)",
-  // "Mr. Smith Goes to Washington (1939)",
-  // "Up (2009)",
-  // "Downfall (2004)",
-  // "Pan's Labyrinth (2006)",
-  // "On the Waterfront (1954)",
-  // "The General (1926)",
-  // "The Seventh Seal (1957)",
-  // "Heat (1995)",
-  // "The Elephant Man (1980)",
-  // "The Maltese Falcon (1941)",
-  // "The Kid (1921)",
-  // "Blade Runner (1982)",
-  // "Scarface (1983)",
-  // "Wild Strawberries (1957)",
-  // "Rebecca (1940)",
-  // "Gran Torino (2008)",
-  // "Fargo (1996)",
-  // "Ran (1985)",
-  // "Ikiru (1952)",
-  // "The Big Lebowski (1998)",
-  // "Touch of Evil (1958)",
-  // "The Gold Rush (1925)",
-  // "The Deer Hunter (1978)",
-  // "Cool Hand Luke (1967)",
-  // "Lock, Stock and Two Smoking Barrels (1998)",
-  // "It Happened One Night (1934)",
-  // "No Country for Old Men (2007)",
-  // "Casino (1995)",
-  // "Sin City (2005)",
-  // "The Sixth Sense (1999)",
-  // "Good Will Hunting (1997)",
-  // "Strangers on a Train (1951)",
-  // "Platoon (1986)",
-  // "Jaws (1975)",
-  // "The Thing (1982)",
-  // "Butch Cassidy and the Sundance Kid (1969)",
-  // "Trainspotting (1996)",
-  // "The Wizard of Oz (1939)",
-  // "Kill Bill: Vol. 1 (2003)",
-  // "Warrior (2011)",
-  // "High Noon (1952)",
-  // "The Secret in Their Eyes (2009)",
-  // "Hotel Rwanda (2004)",
-  // "The Grapes of Wrath (1940)",
-  // "Gone with the Wind (1939)",
-  // "Annie Hall (1977)",
-  // "The Avengers (2012)",
-  // "My Neighbor Totoro (1988)",
-  // "Finding Nemo (2003)",
-  // "V for Vendetta (2005)",
-  // "Notorious (1946)",
-  // "Dial M for Murder (1954)",
-  // "Into the Wild (2007)",
-  // "How to Train Your Dragon (2010)",
-  // "Life of Brian (1979)",
-  // "The Night of the Hunter (1955)",
-  // "There Will Be Blood (2007)",
-  // "Network (1976)",
-  // "The Terminator (1984)",
-  // "Ben-Hur (1959)",
-  // "Million Dollar Baby (2004)",
-  // "The King's Speech (2010)",
-  // "The Big Sleep (1946)",
-  // "Stand by Me (1986)",
-  // "The Hunt (2012)",
-  // "The Best Years of Our Lives (1946)",
-  // "Groundhog Day (1993)",
-  // "The 400 Blows (1959)",
-  // "Twelve Monkeys (1995)",
-  // "Mary and Max (2009)",
-  // "Donnie Darko (2001)",
-  // "Dog Day Afternoon (1975)",
-  // "Howl's Moving Castle (2004)",
-  // "Amores Perros (2000)",
-  // "The Bourne Ultimatum (2007)",
-  // "Persona (1966)",
-  // "Gandhi (1982)",
-  // "The Killing (1956)",
-  // "A Beautiful Mind (2001)",
-  // "Black Swan (2010)",
-  // "The Graduate (1967)",
-  // "The Princess Bride (1987)",
-  // "8½ (1963)",
-  // "Who's Afraid of Virginia Woolf? (1966)",
-  // "The Hustler (1961)",
-  // "La Strada (1954)",
-  // "Rocky (1976)",
-  // "Star Trek Into Darkness (2013)",
-  // "Slumdog Millionaire (2008)",
-  // "Anatomy of a Murder (1959)",
-  // "The Man Who Shot Liberty Valance (1962)",
-  // "The Manchurian Candidate (1962)",
-  // "Rope (1948)",
-  // "The Exorcist (1973)",
-  // "The Wild Bunch (1969)",
-  // "Barry Lyndon (1975)",
-  // "Life of Pi (2012)",
-  // "In the Name of the Father (1993)",
-  // "Stalag 17 (1953)",
-  // "Monsters, Inc. (2001)",
-  // "Infernal Affairs (2002)",
-  // "Fanny and Alexander (1982)",
-  // "Roman Holiday (1953)",
-  // "Sleuth (1972)",
-  // "Memories of Murder (2003)",
-  // "The Truman Show (1998)",
-  // "Pirates of the Caribbean: The Curse of the Black Pearl (2003)",
-  // "Stalker (1979)",
-  // "The Hobbit: An Unexpected Journey (2012)",
-  // "3 Idiots (2009)",
-  // "Like Stars on Earth (2007)",
-  // "Harry Potter and the Deathly Hallows: Part 2 (2011)",
-  // "A Fistful of Dollars (1964)",
-  // "Star Trek (2009)",
-  // "Jurassic Park (1993)",
-  // "Ip Man (2008)",
-  // "The Artist (2011/I)",
-  // "Incendies (2010)",
-  // "District 9 (2009)",
-  // "Ratatouille (2007)",
-  // "A Streetcar Named Desire (1951)",
-  // "La Haine (1995)",
-  // "Shutter Island (2010)",
-  // "The Diving Bell and the Butterfly (2007)",
-  // "Rain Man (1988)",
-  // "All Quiet on the Western Front (1930)",
-  // "Beauty and the Beast (1991)",
-  // "Rosemary's Baby (1968)",
-  // "Nausicaä of the Valley of the Wind (1984)",
-  // "Papillon (1973)",
-  // "Harvey (1950)",
-  // "Mystic River (2003)",
-  // "Before Sunrise (1995)",
-  // "Spring, Summer, Fall, Winter... and Spring (2003)",
-  // "The Untouchables (1987)",
-  // "In the Mood for Love (2000)",
-  // "The Celebration (1998)",
-  // "Manhattan (1979)",
+  "Goodfellas (1990)",
+  "Star Wars (1977)",
+  "Seven Samurai (1954)",
+  "Forrest Gump (1994)",
+  "The Matrix (1999)",
+  "The Lord of the Rings: The Two Towers (2002)",
+  "City of God (2002)",
+  "Se7en (1995)",
+  "The Silence of the Lambs (1991)",
+  "Once Upon a Time in the West (1968)",
+  "Casablanca (1942)",
+  "The Usual Suspects (1995)",
+  "Raiders of the Lost Ark (1981)",
+  "Rear Window (1954)",
+  "It's a Wonderful Life (1946)",
+  "Psycho (1960)",
+  "Léon: The Professional (1994)",
+  "Sunset Blvd. (1950)",
+  "American History X (1998)",
+  "Apocalypse Now (1979)",
+  "Terminator 2: Judgment Day (1991)",
+  "Memento (2000)",
+  "Saving Private Ryan (1998)",
+  "City Lights (1931)",
+  "Dr. Strangelove or: How I Learned to Stop Worrying and Love the Bomb (1964)",
+  "Alien (1979)",
+  "Spirited Away (2001)",
+  "Modern Times (1936)",
+  "North by Northwest (1959)",
+  "Back to the Future (1985)",
+  "Citizen Kane (1941)",
+  "Life Is Beautiful (1997)",
+  "The Shining (1980)",
+  "The Pianist (2002)",
+  "The Departed (2006)",
+  "M (1931)",
+  "Vertigo (1958)",
+  "Django Unchained (2012)",
+  "Paths of Glory (1957)",
+  "The Dark Knight Rises (2012)",
+  "Taxi Driver (1976)",
+  "American Beauty (1999)",
+  "Aliens (1986)",
+  "Double Indemnity (1944)",
+  "The Intouchables (2011)",
+  "WALL·E (2008)",
+  "Gladiator (2000)",
+  "The Green Mile (1999)",
+  "Toy Story 3 (2010)",
+  "The Lives of Others (2006)",
+  "The Great Dictator (1940)",
+  "A Clockwork Orange (1971)",
+  "The Prestige (2006)",
+  "Amélie (2001)",
+  "Lawrence of Arabia (1962)",
+  "To Kill a Mockingbird (1962)",
+  "Reservoir Dogs (1992)",
+  "Das Boot (1981)",
+  "Cinema Paradiso (1988)",
+  "The Lion King (1994)",
+  "The Treasure of the Sierra Madre (1948)",
+  "The Third Man (1949)",
+  "Requiem for a Dream (2000)",
+  "Star Wars: Episode VI - Return of the Jedi (1983)",
+  "Once Upon a Time in America (1984)",
+  "Eternal Sunshine of the Spotless Mind (2004)",
+  "Full Metal Jacket (1987)",
+  "Braveheart (1995)",
+  "Oldboy (2003)",
+  "L.A. Confidential (1997)",
+  "Chinatown (1974)",
+  "Singin' in the Rain (1952)",
+  "Bicycle Thieves (1948)",
+  "Monty Python and the Holy Grail (1975)",
+  "Some Like It Hot (1959)",
+  "Metropolis (1927)",
+  "Princess Mononoke (1997)",
+  "Rashomon (1950)",
+  "Amadeus (1984)",
+  "2001: A Space Odyssey (1968)",
+  "The Sting (1973)",
+  "All About Eve (1950)",
+  "The Apartment (1960)",
+  "Unforgiven (1992)",
+  "Indiana Jones and the Last Crusade (1989)",
+  "Grave of the Fireflies (1988)",
+  "Witness for the Prosecution (1957)",
+  "Raging Bull (1980)",
+  "The Bridge on the River Kwai (1957)",
+  "Die Hard (1988)",
+  "Batman Begins (2005)",
+  "A Separation (2011)",
+  "Yojimbo (1961)",
+  "Inglourious Basterds (2009)",
+  "For a Few Dollars More (1965)",
+  "Snatch. (2000)",
+  "The Great Escape (1963)",
+  "Toy Story (1995)",
+  "Mr. Smith Goes to Washington (1939)",
+  "Up (2009)",
+  "Downfall (2004)",
+  "Pan's Labyrinth (2006)",
+  "On the Waterfront (1954)",
+  "The General (1926)",
+  "The Seventh Seal (1957)",
+  "Heat (1995)",
+  "The Elephant Man (1980)",
+  "The Maltese Falcon (1941)",
+  "The Kid (1921)",
+  "Blade Runner (1982)",
+  "Scarface (1983)",
+  "Wild Strawberries (1957)",
+  "Rebecca (1940)",
+  "Gran Torino (2008)",
+  "Fargo (1996)",
+  "Ran (1985)",
+  "Ikiru (1952)",
+  "The Big Lebowski (1998)",
+  "Touch of Evil (1958)",
+  "The Gold Rush (1925)",
+  "The Deer Hunter (1978)",
+  "Cool Hand Luke (1967)",
+  "Lock, Stock and Two Smoking Barrels (1998)",
+  "It Happened One Night (1934)",
+  "No Country for Old Men (2007)",
+  "Casino (1995)",
+  "Sin City (2005)",
+  "The Sixth Sense (1999)",
+  "Good Will Hunting (1997)",
+  "Strangers on a Train (1951)",
+  "Platoon (1986)",
+  "Jaws (1975)",
+  "The Thing (1982)",
+  "Butch Cassidy and the Sundance Kid (1969)",
+  "Trainspotting (1996)",
+  "The Wizard of Oz (1939)",
+  "Kill Bill: Vol. 1 (2003)",
+  "Warrior (2011)",
+  "High Noon (1952)",
+  "The Secret in Their Eyes (2009)",
+  "Hotel Rwanda (2004)",
+  "The Grapes of Wrath (1940)",
+  "Gone with the Wind (1939)",
+  "Annie Hall (1977)",
+  "The Avengers (2012)",
+  "My Neighbor Totoro (1988)",
+  "Finding Nemo (2003)",
+  "V for Vendetta (2005)",
+  "Notorious (1946)",
+  "Dial M for Murder (1954)",
+  "Into the Wild (2007)",
+  "How to Train Your Dragon (2010)",
+  "Life of Brian (1979)",
+  "The Night of the Hunter (1955)",
+  "There Will Be Blood (2007)",
+  "Network (1976)",
+  "The Terminator (1984)",
+  "Ben-Hur (1959)",
+  "Million Dollar Baby (2004)",
+  "The King's Speech (2010)",
+  "The Big Sleep (1946)",
+  "Stand by Me (1986)",
+  "The Hunt (2012)",
+  "The Best Years of Our Lives (1946)",
+  "Groundhog Day (1993)",
+  "The 400 Blows (1959)",
+  "Twelve Monkeys (1995)",
+  "Mary and Max (2009)",
+  "Donnie Darko (2001)",
+  "Dog Day Afternoon (1975)",
+  "Howl's Moving Castle (2004)",
+  "Amores Perros (2000)",
+  "The Bourne Ultimatum (2007)",
+  "Persona (1966)",
+  "Gandhi (1982)",
+  "The Killing (1956)",
+  "A Beautiful Mind (2001)",
+  "Black Swan (2010)",
+  "The Graduate (1967)",
+  "The Princess Bride (1987)",
+  "8½ (1963)",
+  "Who's Afraid of Virginia Woolf? (1966)",
+  "The Hustler (1961)",
+  "La Strada (1954)",
+  "Rocky (1976)",
+  "Star Trek Into Darkness (2013)",
+  "Slumdog Millionaire (2008)",
+  "Anatomy of a Murder (1959)",
+  "The Man Who Shot Liberty Valance (1962)",
+  "The Manchurian Candidate (1962)",
+  "Rope (1948)",
+  "The Exorcist (1973)",
+  "The Wild Bunch (1969)",
+  "Barry Lyndon (1975)",
+  "Life of Pi (2012)",
+  "In the Name of the Father (1993)",
+  "Stalag 17 (1953)",
+  "Monsters, Inc. (2001)",
+  "Infernal Affairs (2002)",
+  "Fanny and Alexander (1982)",
+  "Roman Holiday (1953)",
+  "Sleuth (1972)",
+  "Memories of Murder (2003)",
+  "The Truman Show (1998)",
+  "Pirates of the Caribbean: The Curse of the Black Pearl (2003)",
+  "Stalker (1979)",
+  "The Hobbit: An Unexpected Journey (2012)",
+  "3 Idiots (2009)",
+  "Like Stars on Earth (2007)",
+  "Harry Potter and the Deathly Hallows: Part 2 (2011)",
+  "A Fistful of Dollars (1964)",
+  "Star Trek (2009)",
+  "Jurassic Park (1993)",
+  "Ip Man (2008)",
+  "The Artist (2011/I)",
+  "Incendies (2010)",
+  "District 9 (2009)",
+  "Ratatouille (2007)",
+  "A Streetcar Named Desire (1951)",
+  "La Haine (1995)",
+  "Shutter Island (2010)",
+  "The Diving Bell and the Butterfly (2007)",
+  "Rain Man (1988)",
+  "All Quiet on the Western Front (1930)",
+  "Beauty and the Beast (1991)",
+  "Rosemary's Baby (1968)",
+  "Nausicaä of the Valley of the Wind (1984)",
+  "Papillon (1973)",
+  "Harvey (1950)",
+  "Mystic River (2003)",
+  "Before Sunrise (1995)",
+  "Spring, Summer, Fall, Winter... and Spring (2003)",
+  "The Untouchables (1987)",
+  "In the Mood for Love (2000)",
+  "The Celebration (1998)",
+  "Manhattan (1979)",
 ]
 
 class Movie extends React.Component {
@@ -306,21 +306,21 @@ class Movie extends React.Component {
 }
 
 
-class Owner extends React.Component {
-  constructor(props){
-    super(props);
-  }
+// class Owner extends React.Component {
+//   constructor(props){
+//     super(props);
+//   }
 
-  render() {
-    return (
-      <div className="row">
-        <div id="owner-info" className="col-sm-12">
-          <a href="#" className="button-style" onClick={this.props.resetAll}>Reset All</a>
-        </div>
-      </div>
-    );
-  }
-}
+//   render() {
+//     return (
+//       <div className="row">
+//         <div id="owner-info" className="col-sm-12">
+//           <a href="#" className="button-style" onClick={this.props.resetAll}>Reset All</a>
+//         </div>
+//       </div>
+//     );
+//   }
+// }
 
 class Movies extends React.Component {
   constructor(){
@@ -343,50 +343,23 @@ class Movies extends React.Component {
   }
 
   deleteMovie(id) {
-    console.log("delete movie")
+    // console.log("delete movie")
     superagent
       .delete(`/i/docs/movies/${id}.json`)
       .then(res => {
-        console.log("delete!" + id);
+        // console.log("delete!" + id);
       });
   }
 
-  resetAll() {
-    // console.log("resetAll now");
-    // var shouldDelete = []
-    // // var addedMovies = []
-
-    // for (let movie of this.state.movies) {
-    //   console.log(movie.name)
-    //   var index = moviesOrder.indexOf(movie.name)
-    //   if (index == -1) {
-    //     console.log("should delete")
-    //     console.log(movie)
-    //     shouldDelete.push(movie.id);
-    //     this.deleteMovie(movie.id)
-    //   }
-    //   else {
-    //     // addedMovies.push(movie.name);
-    //   }
-    // }
-
-    // console.log("shouldDelete")
-    // console.log(shouldDelete)
-    // console.log("addedMovies")
-    // console.log(addedMovies)
-  }
-
-  orderMovies() {
-    var moviesSet = [];
-    for (let movie of res.body) {
+  orderMovies(movieDict) {
+    var movieOrdered = [];
+    for (let movie of movieDict) {
       var index = moviesOrder.indexOf(movie.name)
       if (index != -1) {
-        moviesSet[index] = movie
+        movieOrdered[index] = movie;
       }
-      this.setState({
-        movies: moviesSet,
-      })
     }
+    return movieOrdered;
   }
 
   updateMovies() {
@@ -398,32 +371,27 @@ class Movies extends React.Component {
           alert("API Failed");
         }
 
-        var moviesSet = [];
+        this.setState({
+          movies: this.orderMovies(res.body),
+        })
+
         for (let movie of res.body) {
           var index = moviesOrder.indexOf(movie.name)
-          if (index != -1) {
-            moviesSet[index] = movie
-          }
-          else {
+          if (index == -1) {
             this.deleteMovie(movie.id)
           }
         }
 
-        this.setState({
-          movies: moviesSet,
-        })
-
-        // console.log(res.body);
         var apiMovies = []
         for (let movie in res.body) {
-          // console.log(res.body[movie]);
           apiMovies.push(res.body[movie].name);
         }
-        // console.log(apiMovies);
 
-        for (let movie in moviesOrder) {
-          if (apiMovies.indexOf(moviesOrder[movie]) == -1) {
-            this.createMovie(moviesOrder[movie]);
+        // console.log(apiMovies);
+        for (let movie of moviesOrder) {
+          // console.log(movie);
+          if (apiMovies.indexOf(movie) == -1) {
+            this.createMovie(movie);
           }
         }
       });
@@ -452,13 +420,6 @@ class Movies extends React.Component {
       )
     );
 
-    var owner = ""
-    if (this.state.owner == true) {
-      var owner = (
-        <Owner resetAll={this.resetAll.bind(this)} />
-      )
-    }
-
     if (this.state.owner == true) {
       var className = ["col-sm-6 offset-sm-3 owner"]
     }
@@ -473,7 +434,6 @@ class Movies extends React.Component {
             <strong>250</strong>
             IMDB Top Movies of all Time.
           </h1>
-          {owner}
           <ul id="movies" className="list-unstyled">
             {movieItems}
           </ul>
